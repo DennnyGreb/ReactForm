159e61d1dc10288af1482c446c501282d542740f
DennyGreb <dendendengrebenets@gmail.com>
2016-10-10 23:55:48 +0300

    Add two-way databinding for inputs

--- app.jsx
+++ app.jsx
@@ -1,33 +1,167 @@
-var Hero = React.createClass({
-    getInitialState: function() {
-        return {
-            count: 0
-        };
-    },
-
-    handleClick: function () {
-        this.setState({ count: this.state.count + 1 });
-    },
-
-    render: function() {
+var dict = []
+class FormComponent extends React.Component {
+    render() {
         return (
-            <div className="container">
-                <div className="count">{this.state.count}</div>
-                <img src={this.props.imageUrl} onClick={this.handleClick} />
-                <h1>{this.props.title}</h1>
-                <p>{this.props.subtitle}</p>
-            </div>
+        <form className="form-horizontal">
+            <fieldset>
+                <Legend/>
+                <UserName name="Please, enter your super full name"/>
+                <Email name="Please, provide your E-mail"/>
+                <Pass name="Please, enter password"/>
+                <PassConfirm name="Please, confirm password"/>
+                <SuccessButton/>
+            </fieldset>
+        </form>
         );
     }
-});
+}
 
-ReactDOM.render(
-    <div>
-        <Hero title="React"
-            subtitle="Библиотека для создания пользовательских интерфейсов"
-            imageUrl="https://facebook.github.io/react/img/logo.svg" />
-        <Hero title="Angular 2"
-            subtitle="Один фреймворк"
-            imageUrl="https://angular.io/resources/images/logos/angular2/angular.svg" />
-    </div>,
-document.getElementById('root'));
+class Legend extends React.Component {
+    render() {
+        return(
+        <div id="legend">
+            <legend>Register</legend>
+        </div>
+        );
+    }
+}
+
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+class UserName extends React.Component {
+    constructor(props){
+    super(props);    
+    this.state = {full_name: 'Your beautiful full name here!'};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({full_name: event.target.value});
+        dict.push({
+            full_name: this.state.full_name
+        })
+        console.log(dict)
+    }
+
+    render() {
+        return(
+        <div className="control-group">
+            <label className="control-label" htmlFor="username">Username</label>
+            <div className="controls">
+                <input type="text" id="username" name="full_name"
+                 className="input-xlarge" value={this.state.full_name} onChange={this.handleChange}/>
+                <p className="help-block">{this.props.name}</p>
+            </div>
+        </div>
+        );
+    }
+}
+
+
+class Email extends React.Component {
+    constructor(props) {
+    super(props);    
+    this.state = {email: 'Your perfect email here!'};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({email: event.target.value});
+        console.log(this.state)
+    }
+    render() {
+        return (
+        <div className="control-group">
+            <label className="control-label" htmlFor="email"></label>
+            <div className="controls">
+                <input type="text" id="email" name="email" 
+                className="input-xlarge" value={this.state.email} onChange={this.handleChange}/>
+                <p className="help-block">{this.props.name}</p>
+            </div>
+        </div>
+        );
+    }
+}
+
+class Pass extends React.Component {
+    constructor(props) {
+    super(props);    
+    this.state = {password: ''};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({password: event.target.value});
+        console.log(this.state)
+    }
+    render() {
+        return (
+        <div className="control-group">
+            <label className="control-label" htmlFor="password"></label>
+            <div className="controls">
+                <input type="password" id="password" name="password" className="input-xlarge" 
+                value={this.state.password} onChange={this.handleChange}/>
+                <p className="help-block">{this.props.name}</p>
+            </div>
+        </div>
+        );
+    }
+}
+
+class PassConfirm extends React.Component {
+    constructor(props) {
+    super(props);    
+    this.state = {password_confirm: ''};
+    this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({password_confirm: event.target.value});
+        console.log(this.state)
+    }
+    render() {
+        return (
+        <div className="control-group">
+            <label className="control-label"  htmlFor="password_confirm"></label>
+            <div className="controls">
+                <input type="password" id="password_confirm" name="password_confirm" className="input-xlarge" 
+                value={this.state.password_confirm} onChange={this.handleChange}/>
+                <p className="help-block">{this.props.name}</p>
+            </div>
+        </div>
+        );
+    }
+}
+
+class SuccessButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+        <div className="control-group">
+            <div className="controls">
+                <button className="btn btn-success">Register</button>
+            </div>
+        </div>
+        );
+    }
+}
+
+ReactDOM.render(<FormComponent name="Please, enter your super full name!)"/>, document.getElementById('registration'));
